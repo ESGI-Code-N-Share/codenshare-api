075aea2d1e9803a8f132bf5ba7d9613cbc8a4ece
corentin lechêne <corentin.lechene.pro@gmail.com>
2024-06-16 22:32:09 +0200

    add test to entities

---
+++ tests/unit/infrastructure/orm/lucid/entities/message_entity.spec.ts
@@ -0,0 +1,51 @@
+import { test } from '@japa/runner'
+import * as sinon from 'sinon'
+import { DateTime } from 'luxon'
+import MessageEntity from '#infrastructure/orm/lucid/entities/message_entity'
+import { Message } from '#domains/users/conversations/messages/message_model'
+import UserEntity from '#infrastructure/orm/lucid/entities/user_entity'
+import { User } from '#domains/users/user_model'
+import type { BelongsTo } from '@adonisjs/lucid/types/relations'
+
+test.group('MessageEntity', () => {
+  test('should convert to domain', async ({ assert }) => {
+    const sendAt = DateTime.now()
+    const sender: User = {
+      userId: '1',
+      firstname: 'John',
+      lastname: 'Doe',
+      email: 'john.doe@email.com',
+      birthdate: new Date('1990-01-01'),
+      avatar: 'avatar.png',
+      role: 'user',
+      overview: '',
+      password: '',
+      createdAt: DateTime.now().toJSDate(),
+      token: 'token',
+    }
+    const expected: Message = {
+      messageId: '1',
+      content: 'Hello, World!',
+      sender: sender,
+      image: undefined,
+      conversationId: '1',
+      sendAt: sendAt.toJSDate(),
+    }
+
+    const userEntityStub = sinon.stub(UserEntity.prototype, 'toDomain').returns(sender)
+
+    const messageEntity = new MessageEntity()
+    messageEntity.messageId = expected.messageId
+    messageEntity.content = expected.content
+    messageEntity.senderId = expected.sender.userId
+    messageEntity.sender = new UserEntity() as BelongsTo<typeof UserEntity>
+    messageEntity.conversationId = expected.conversationId
+    messageEntity.sendAt = sendAt
+
+    const result = messageEntity.toDomain()
+
+    assert.deepEqual(result, expected)
+
+    userEntityStub.restore()
+  })
+})
---
+++ tests/unit/infrastructure/orm/lucid/entities/post_entity.spec.ts
@@ -0,0 +1,67 @@
+import { test } from '@japa/runner'
+import * as sinon from 'sinon'
+import { DateTime } from 'luxon'
+import { User } from '#domains/users/user_model'
+import Post from '#domains/posts/post_model'
+import { PostLike } from '#domains/posts/post_like/post_like_model'
+import { PostLikeEntity } from '#infrastructure/orm/lucid/entities/post_like_entity'
+import UserEntity from '#infrastructure/orm/lucid/entities/user_entity'
+import PostEntity from '#infrastructure/orm/lucid/entities/post_entity'
+import { BelongsTo, HasMany } from '@adonisjs/lucid/types/relations'
+
+test.group('PostEntity', () => {
+  test('should convert to domain', async ({ assert }) => {
+    const postId = '1'
+    const postedAt = DateTime.now()
+    const author: User = {
+      userId: '1',
+      firstname: 'John',
+      lastname: 'Doe',
+      email: 'john.doe@email.com',
+      birthdate: new Date('1990-01-01'),
+      avatar: 'avatar.png',
+      role: 'user',
+      overview: '',
+      password: '',
+      createdAt: DateTime.now().toJSDate(),
+      token: 'token',
+    }
+    const likes: PostLike[] = [
+      {
+        postLikeId: '1',
+        userId: author.userId,
+        postId: postId,
+        likedAt: DateTime.now().toJSDate(),
+      },
+    ]
+
+    const expected: Post = {
+      postId: postId,
+      title: 'Hello, World!',
+      content: 'Hello, World!',
+      author: author,
+      postedAt: postedAt.toJSDate(),
+      image: undefined,
+      likes: likes,
+    }
+
+    const userEntityStub = sinon.stub(UserEntity.prototype, 'toDomain').returns(author)
+    const postLikeEntityStub = sinon.stub(PostLikeEntity.prototype, 'toDomain').returns(likes[0])
+
+    const postEntity = new PostEntity()
+    postEntity.postId = expected.postId
+    postEntity.title = expected.title
+    postEntity.content = expected.content
+    postEntity.authorId = expected.author.userId
+    postEntity.author = new UserEntity() as BelongsTo<typeof UserEntity>
+    postEntity.likes = [new PostLikeEntity()] as HasMany<typeof PostLikeEntity>
+    postEntity.postedAt = postedAt
+
+    const result = postEntity.toDomain()
+
+    assert.deepEqual(result, expected)
+
+    userEntityStub.restore()
+    postLikeEntityStub.restore()
+  })
+})
